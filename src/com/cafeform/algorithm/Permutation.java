3789fc033600b6d4dd88f5c71e0886a1dcf0ed6f
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2013-02-02 22:56:47 +0900

    Mod: correct indent

--- src/com/cafeform/algorithm/Permutation.java
+++ src/com/cafeform/algorithm/Permutation.java
@@ -5,43 +5,40 @@ import java.util.*;
 /**
  * Experimental implementation of Permutation
  */
-public class Permutation<T>
-{
-	private List<T> list;
-	private int numChosen;
-	List<List<T>> result = new LinkedList<List<T>>();
+public class Permutation<T> {
 
-	public Permutation(List<T> list, int numChosen)
-	{
-		this.list = list;
-		this.numChosen = numChosen;
-	}
+    private List<T> list;
+    private int numChosen;
+    List<List<T>> result = new LinkedList<List<T>>();
 
-	public List<List<T>> getResult()
-	{
-		permutation(new ArrayList<Integer>(), 0);
-		return result;
-	}
+    public Permutation(List<T> list, int numChosen) {
+        this.list = list;
+        this.numChosen = numChosen;
+    }
 
-	private void permutation(ArrayList<Integer> runningUsage, int runningDepth)
-	{
-		if(runningDepth == numChosen){
-			List<T> candidateList = new ArrayList<T>();
-			for(Integer i : runningUsage){
-				 candidateList.add(list.get(i));
-			}
-			result.add(candidateList);
-			return;
-		}
-		for(int index = 0 ; index < list.size() ; index++){
-			/*
-			 * New index must not be appered before.
-			 */
-			if( !runningUsage.contains(index)){
-				runningUsage.add(index);
-				permutation(runningUsage, runningDepth + 1);
-				runningUsage.remove(runningUsage.size() -1);
-			}
-		}
-	}
+    public List<List<T>> getResult() {
+        permutation(new ArrayList<Integer>(), 0);
+        return result;
+    }
+
+    private void permutation(ArrayList<Integer> runningUsage, int runningDepth) {
+        if (runningDepth == numChosen) {
+            List<T> candidateList = new ArrayList<T>();
+            for (Integer i : runningUsage) {
+                candidateList.add(list.get(i));
+            }
+            result.add(candidateList);
+            return;
+        }
+        for (int index = 0; index < list.size(); index++) {
+            /*
+             * New index must not be appered before.
+             */
+            if (!runningUsage.contains(index)) {
+                runningUsage.add(index);
+                permutation(runningUsage, runningDepth + 1);
+                runningUsage.remove(runningUsage.size() - 1);
+            }
+        }
+    }
 }
--- src/com/cafeform/algorithm/PermutationTestMain.java
+++ src/com/cafeform/algorithm/PermutationTestMain.java
@@ -6,22 +6,21 @@ import java.util.List;
 /**
  * Test main class for Permutation class
  */
-public class PermutationTestMain
-{
-	public static void main(String args[]){
-		new PermutationTestMain().start();
-	}
+public class PermutationTestMain {
 
-	private void start()
-	{
-		int num = 3;
-		List<String> list = Arrays.asList("a", "b", "c", "d", "e");
-		List<List<String>> result;
+    public static void main(String args[]) {
+        new PermutationTestMain().start();
+    }
 
-		Permutation permutation = new Permutation(list, num);
-		result = permutation.getResult();
+    private void start() {
+        int num = 3;
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e");
+        List<List<String>> result;
 
-		System.out.println("Count: " + result.size());
-		System.out.println(result);
-	}
+        Permutation permutation = new Permutation(list, num);
+        result = permutation.getResult();
+
+        System.out.println("Count: " + result.size());
+        System.out.println(result);
+    }
 }
